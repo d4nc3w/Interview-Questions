260b15880e86fc18458125cdfbffc8db45aa6fff
Piotr Dancewicz <s28113@pjwstk.edu.pl>
2024-10-16 15:09:44 +0200

    Added rotateArray solution

---
+++ Questions/RotateArray.java
@@ -0,0 +1,28 @@
+package Questions;
+
+public class RotateArray {
+    public static int[] rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+        int[] rotated = new int[n];
+
+        for(int i = 0 ; i < n; i++){
+            rotated[(i + k) % n] = nums[i];
+        }
+        for(int i = 0; i < n; i++){
+            nums[i] = rotated[i];
+        }
+
+        return rotated;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6,7};
+        int k = 3;
+        int[] solution = rotate(nums, k);
+        for(int num : solution){
+            System.out.println(num);
+        }
+        
+    }
+}
