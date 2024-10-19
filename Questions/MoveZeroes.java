e397b843b68d06d47a4e4cc35f977db602f2e119
Piotr Dancewicz <s28113@pjwstk.edu.pl>
2024-10-19 15:11:30 +0200

    Added TwoSum solution

--- Questions/MoveZeroes.java
+++ Questions/MoveZeroes.java
@@ -18,6 +18,8 @@ public class MoveZeroes {
         return nums;
     }
 
+    //   Time Complexity: O(n)   Space Complexity: O(n)
+
     public static void main(String[] args) {
         int[] nums = {0,1,0,3,12};
         int[] result = moveZeroes(nums);
---
+++ Questions/TwoSum.java
@@ -0,0 +1,26 @@
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//You can return the answer in any order.
+
+package Questions;
+
+public class TwoSum {
+    public static int[] twoSum(int[] nums, int target) {
+        for(int i = 0; i < nums.length; i++){
+            for(int j = i + 1; j < nums.length; j++){
+                if(nums[i] + nums[j] == target){
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int[] result = twoSum(nums, 9);
+        for(int n : result){
+            System.out.println(n);
+        }
+    }
+}
