79d7e58153f33f11470ed936fc445ae01396fd8c
Piotr Dancewicz <s28113@pjwstk.edu.pl>
2024-10-23 19:05:28 +0200

    Small fix

--- Questions/LinkedList/DeleteNode.java
+++ Questions/LinkedList/DeleteNode.java
@@ -1,13 +1,13 @@
-package Questions.String;
+package Questions.LinkedList;
 
 //Definition for singly-linked list.
-public class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) { 
-        val = x; 
-    }
-}
+// public class ListNode {
+//     int val;
+//     ListNode next;
+//     ListNode(int x) { 
+//         val = x; 
+//     }
+// }
  
 
 public class DeleteNode {
---
+++ Questions/LinkedList/ListNode.java
@@ -0,0 +1,9 @@
+package Questions.LinkedList;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { 
+        val = x; 
+    }
+}
