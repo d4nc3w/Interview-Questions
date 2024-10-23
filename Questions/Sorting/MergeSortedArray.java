7009a0e945f618a482e0367b4169c724ba0f4fd6
Piotr Dancewicz <s28113@pjwstk.edu.pl>
2024-10-23 19:29:09 +0200

    Added MergeSortedArrays solution

---
+++ Questions/Sorting/MergeSortedArray.java
@@ -0,0 +1,12 @@
+package Questions.Sorting;
+
+public class MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        
+    }
+
+    public static void main(String[] args) {
+        int[] nums1  = {1, 2, 3};
+        int[] nums2 = {2,5}
+    }
+}
